a5093a078f383fb8f0d63e9a535c5fffd3eddecb
rominadouk <douk.romina@gmail.com>
2023-06-04 00:52:05 -0700

    added collapsable navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,25 @@ import React from "react";
 
 const Navbar = () => {
     return ( 
-        <>
-            <h1 className="Navbar-brand-name">FoodSpot</h1>
-            <ul>
-                <li>Home</li>
-                <li>Search Food Spots</li>              
-            </ul>
-
-        </>
+        <div>
+            <nav className="navbar navbar-expand-lg">
+                <a className="navbar-brand" href="#">FOODSPOT</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto ">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Find a Food Spot</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
      );
-}
+};
  
 export default Navbar;
