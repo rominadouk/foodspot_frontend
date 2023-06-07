f240578bf9a165bc9721cadab493905f4e6a4342
rominadouk <douk.romina@gmail.com>
2023-06-07 16:18:04 -0700

    adjusted just visited

--- src/components/JustVisited.tsx
+++ src/components/JustVisited.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import './HomePage.css'
 
 
 const JustVisited = () => {
     return ( 
         <>
-            <h2>Just Visited</h2>
+            <h2 className='text-center mt-3 just-visited'>Just Visited</h2>
+            <div className='just-visited-image'></div>
             <h3>Food Spot</h3>
             <p>City, State</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolorem deserunt ducimus reiciendis voluptates earum sequi quae! Voluptatibus iusto sit adipisci voluptatum neque magnam reiciendis consectetur. Aspernatur ipsa nostrum optio.</p>
