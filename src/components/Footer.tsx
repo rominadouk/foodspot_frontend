68bd92898647101b8359ffce656a9f2a8c5eee6a
rominadouk <douk.romina@gmail.com>
2023-07-01 02:21:04 -0700

    added footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Twitter, Facebook, Github, Pinterest } from "react-bootstrap-icons";
+
 
 
 const Footer = () => {
     return ( 
-        <>
-            <p>Instagram</p>
-            <p>Facebook</p>
-            <p><i className="bi bi-twitter"></i>Twitter</p>
-            <p>Pinterest</p>
-        </>
+        <div className="icon-div">
+            <Container className="icon-container">
+                <Row>
+                    <Col className="col-lg-1">
+                        <Facebook className="icon"/>
+                    </Col>
+                    <Col className="col-lg-1">
+                        <Github className="icon"/>
+                    </Col>
+                    <Col className="col-lg-1">
+                        <Twitter className="icon"/>
+                    </Col>
+                    <Col className="col-lg-1">
+                        <Pinterest className="icon"/>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
      );
 }
  
